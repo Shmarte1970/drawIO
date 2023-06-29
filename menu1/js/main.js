85a9d7dadb32903a89a5f6ae68167b6484c81d95
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-06-29 09:00:57 +0200

    cambio menu1 js poniendo primero horas

--- menu1/js/main.js
+++ menu1/js/main.js
@@ -1,7 +1,17 @@
-function updateTime() {
+/*function updateTime() {
     var now = new Date();
     var datetimeElement = document.getElementById('datetime');
     datetimeElement.textContent = now.toLocaleString('es-ES', { hour12: false });
+}*/
+
+
+function updateTime() {
+    var now = new Date();
+    var datetimeElement = document.getElementById('datetime');
+    var options = { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    var timeString = now.toLocaleTimeString('es-ES', options);
+    var dateString = now.toLocaleDateString('es-ES');
+    datetimeElement.textContent = timeString + ' - ' + dateString;
 }
 
 updateTime();
