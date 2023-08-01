ffeb85425977dc48b549959e279ff25033e62fa5
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-08-01 14:25:42 +0200

    PrimerServlet con Luisina

---
+++ PrimerServlet/PrimerServlet/src/main/java/com/java/web/primerservlets/svUsuarios.java
@@ -0,0 +1,45 @@
+
+package com.java.web.primerservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Pedro Ríos
+ */
+@WebServlet(name = "svUsuarios", urlPatterns = {"/svUsuarios"})
+public class svUsuarios extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     
+    }
+
+ 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
