8b1d6b7ea361af68ddeac59b44e7ac98296f4184
Shmarte1970 <shmarte@gmail.com>
2023-08-04 10:20:35 +0200

    Odontologo

--- test/testJPA/src/Logica/testjpa.java
+++ test/testJPA/src/Logica/testjpa.java
@@ -15,9 +15,9 @@ public class testjpa {
 
     public static void main(String[] args) {
         
-        /*Alumno alumno = new Alumno("52391115J", "Pedro", "Rios Macias", new Date("05/02/1970"));
+        Alumno alumno = new Alumno("52391115J", "Pedro", "Rios Macias", new Date("05/02/1970"));
         Alumno alumno2 = new Alumno("00000023T", "Armando", "Bronca Segura", new Date("06/12/1978"));
-       Alumno alumno3 = new Alumno("00000000Z", "John", "Weck", new Date("1986/04/15"));*/
+       Alumno alumno3 = new Alumno("00000000Z", "John", "Weck", new Date("1986/04/15"));
         
         ControladoraPersistencia control = new ControladoraPersistencia();
         
@@ -28,8 +28,11 @@ public class testjpa {
          for (Alumno alu:listaAlumnos){
              System.out.println(alu.getDni()+" "+alu.getNombre()+" "+alu.getApellido());
          }
+        */
+        control.crearAlumno(alumno);
+        control.crearAlumno(alumno2);
+        control.crearAlumno(alumno3);
         
-        /*control.crearAlumno(alumno3);*/
         
         /*control.eliminarAlumno("00000000Z");
         
