46dd282d324eea942b023f246776aba6be5af7d8
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-08-07 14:27:44 +0200

    lombok

--- Odontologo/src/main/java/logica/Horario.java
+++ Odontologo/src/main/java/logica/Horario.java
@@ -3,8 +3,9 @@ package logica;
 
 import java.util.Date;
 import javax.persistence.Id;
+import lombok.Data;
 
-
+@Data
 public class Horario {
     
     @Id
@@ -12,39 +13,7 @@ public class Horario {
     private String horario_inicio;
     private String horario_fin;
 
-    public Horario() {
-    }
 
-    public Horario(Integer id_horario, String horario_inicio, String horario_fin) {
-        this.id_horario = id_horario;
-        this.horario_inicio = horario_inicio;
-        this.horario_fin = horario_fin;
-    }
-
-    public Integer getId_horario() {
-        return id_horario;
-    }
-
-    public void setId_horario(Integer id_horario) {
-        this.id_horario = id_horario;
-    }
-
-   
-
-    public String getHorario_inicio() {
-        return horario_inicio;
-    }
-
-    public void setHorario_inicio(String horario_inicio) {
-        this.horario_inicio = horario_inicio;
-    }
-
-    public String getHorario_fin() {
-        return horario_fin;
-    }
-
-    public void setHorario_fin(String horario_fin) {
-        this.horario_fin = horario_fin;
-    }
+    
     
 }
