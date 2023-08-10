c39362ef63403795cbc3f90008277f6d509127e9
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-08-10 13:00:36 +0200

    zmapeoTest con la bbdd tempo(3).sql

---
+++ zmapeotest/zmapeoTest/src/main/java/logicas/produc.java
@@ -0,0 +1,36 @@
+
+package logicas;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author Pedro Ríos
+ */
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class produc {
+    
+    @Id    
+    private int id_produc;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+    private String email;
+    private String cp;
+    private String poblacion;
+    private String provincia;
+    
+    
+    
+}
