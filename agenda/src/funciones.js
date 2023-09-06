7a77eb92682c0da51e545f18d88fa54b8395a004
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-09-06 08:02:57 +0200

    Update UML Colvellog V3

--- agenda/src/funciones.js
+++
@@ -1,7 +0,0 @@
-const nombre = document.querySelector('#nombre')
-const numero = document.querySelector('#numero')
-const btnAgregar = document.querySelector('#btn_agregar')
-
-btnAgregar.addEventListener('click',function(){
-    window.location.href = `agregar/${nombre.value}/${numero.value}`
-})
