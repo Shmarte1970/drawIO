0379aeafef2afd69c1001db6564770a12ece110b
Shmarte1970 <shmarte@gmail.com>
2023-09-22 13:37:39 +0200

    eliminacion del crud

--- Angular/Crud/frontEnd/src/app/interfaces/empresa.ts
+++
@@ -1,9 +0,0 @@
-export interface Empresa {
-    id?:number;
-    usuario:string;   
-    telefono:string;    
-    poblacion:string;
-    dadoDeAlta:string;
-    
-
-}
--- Angular/Crud/server/dist/db/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('prUsuarios', 'root', 'root2023', {
-    /*Acceso a BBDD de AWS */
-    host: 'testbbdd.c00tjquuyvht.eu-north-1.rds.amazonaws.com',
-    dialect: 'mysql'
-});
-exports.default = sequelize;
